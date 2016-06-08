0d59828dd917ed59e222470edfd4b804e224a2aa
Phi Tran <phitranphitranphitran@gmail.com>
2016-06-08 00:11:25 -0400

    Use async, add beforeEach and afterEach user data to user model test

--- test/models/user-model-test.js
+++ test/models/user-model-test.js
@@ -2,9 +2,39 @@
 
 const expect = require("chai").expect;
 const mongoose = require("mongoose");
+const async = require("async");
 const User = require("../../app/models/user-model");
 
 describe("User Model", () => {
+
+  // before(done => {
+  //   mongoose.connect(process.env.MONGO_URI);
+  //   done();
+  // });
+
+  beforeEach(done => {
+    const users = [
+      new User({
+        profile: { name: "johnny" },
+        email: "johnny@johnny.com",
+        password: "johnnyisthebest"
+      }),
+      new User({
+        profile: { name: "bobby" },
+        email: "bobby@bobby.com",
+        password: "bobbyisthebest"
+      }),
+      new User({
+        profile: { name: "tommy" },
+        email: "tommy@tommy.com",
+        password: "tommyisthebest"
+      })
+    ];
+    // users.forEach(user => user.save());
+    // done();
+    // const tasks = users.map(user => () => user.save());
+    async.parallel(users.map(user => user.save), done);
+  });
 
   it("should create a new user", (done) => {
     const user = new User({
@@ -13,7 +43,7 @@ describe("User Model", () => {
       password: "password"
     });
     user.save((err, user) => {
-      expect(err).to.be.null;
+      expect(err).to.not.be.defined;
       expect(user.profile.name).to.equal("MrTest");
       expect(user.email).to.equal("test@gmail.com");
       expect(user).to.have.property("createdAt");
@@ -22,10 +52,10 @@ describe("User Model", () => {
     });
   });
 
-  it("should not create a user with the unique email", (done) => {
+  it("should not create a user with duplicate email", (done) => {
     const user = new User({
-      profile: { name: "MrTest" },
-      email: "test@gmail.com",
+      profile: { name: "MrCopycat" },
+      email: "bobby@bobby.com", // duplicate email
       password: "password"
     });
     user.save((err) => {
@@ -36,17 +66,22 @@ describe("User Model", () => {
   });
 
   it("should find user by email", (done) => {
-    User.findOne({ email: "test@gmail.com" }, (err, user) => {
+    User.findOne({ email: "bobby@bobby.com" }, (err, user) => {
       expect(err).to.be.null;
-      expect(user.email).to.equal("test@gmail.com");
+      expect(user.profile.name).to.equal("bobby");
+      expect(user.email).to.equal("bobby@bobby.com");
       done();
     });
   });
 
   it("should delete a user", (done) => {
-    User.remove({ email: "test@gmail.com" }, (err) => {
+    User.remove({ email: "bobby@bobby.com" }, (err) => {
       expect(err).to.be.null;
-      done();
+      // done();
+      User.findOne({ email: "bobby@bobby.com" }, (err, user) => {
+        expect(user).to.not.be.defined;
+        done();
+      });
     });
   });
 
@@ -57,7 +92,13 @@ describe("User Model", () => {
     });
     user.save((err, user) => {
       expect(err).to.be.defined;
-      expect(user).to.be.undefined;
+      expect(user).to.not.be.defined;
+      done();
+    });
+  });
+
+  afterEach(done => {
+    User.remove({}, (err) => {
       done();
     });
   });
